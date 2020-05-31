b7f42c80f63f9eff1a97a15bfff4ac13eefd9b23
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-05-31 10:44:54 -0700

    Broken Code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# WEB231-Discussion1.1
+## WEB231-Discussion1.1
+
+> Discussion 1.1
+>
+> Goal: Code talks about difference between comparison and string interpolation.
+>
+> Code Contains multiple errors
+
+**To run code**
+&nbsp;&nbsp;&nbsp;&nbsp;`node index.js`
+
+**Feel Free to fork/clone/copy**
---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+    Discussion 1.1
+    Author Anil Rayamajhi
+
+    Goal: Code talks about difference between comparison and string interpolation.
+
+    Note: Code contains ES6 syntax
+    Code Contain multiple errors
+ */
+
+
+const constOne = "5",
+  constTwo = 25,
+  fullScore = 100;
+
+let varOne = 5,
+  varTwo;
+
+clg("Check the value of varTwo: ", varTwo);
+
+const isTrueOne = constOne === ConstTWO / 5;
+const isTrueTwo = constOne == ConstTWO / 5;
+
+console.log("Is True: " isTrueOne);
+console.log(`Is True: ${isTrueTwo}`);
+
+if (isTrueOne) {
+  //HINT: correct validator variable is needed to log this message
+  // Log value of `fullScore`
+  log(`Congratulations! You successfully hacked. Your score {fullScore}%`);
+}
