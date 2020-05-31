5bdb63cfd853c03df9894221bc54ce30b7d65ac0
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-05-31 10:57:09 -0700

    Marie Barleta - corrected code

--- marieBarleta.js
+++ marieBarleta.js
@@ -1,18 +1,16 @@
 /* ============================================ 
 
 ;Title: Discussion 1.1
-; Author: Nicole Barleta
-; Date: 27 May 2020
-; Description: This snippet of code is an example of declaration of variables in JavaScript
-;it contains multiple errors to be adjusted by my fellow students
+; Author: Anil Rayamajhi
 
-;=========================================== */ 
-
+; Corrected Code
+;=========================================== */
 
 // This code contains multiple errors
-var num1 = 7, num2 =
+var num1 = 7,
+  num2 = 1;
 var sum = num1 + num2;
-var diff = 14-7
-var answer = "the difference is " diff
-console.log("the total is " +\"sum");
+var diff = 14 - 7;
+var answer = "the difference is " + diff;
+console.log("the total is " + sum);
 console.log(answer);
